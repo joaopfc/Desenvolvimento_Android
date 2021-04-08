2dc747586e27938ec9b60994482e66c78b8b1a0d
joaopfc <joaopaulofariadecarvalho@gmail.com>
2021-04-08 20:41:45 -0300

    Realizado aula basica de SQLite

---
+++ AppBancoDeDadosMeusClientes/app/src/main/java/joaopfc/appbancodedadosmeusclientes/MainActivity.java
@@ -0,0 +1,16 @@
+package joaopfc.appbancodedadosmeusclientes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppDataBase appDataBase = new AppDataBase(this);
+    }
+}
